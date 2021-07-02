b817ec73f86b8bcc9a096e539c00a3be43ba4190
dz0nda <dzonda@student.42.fr>
2021-07-02 05:58:17 +0200

    clean: entry app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
 import {Provider} from 'react-redux';
 import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
 import {EvaIconsPack} from '@ui-kitten/eva-icons';
@@ -16,15 +15,3 @@ export default (): React.ReactFragment => (
     </ApplicationProvider>
   </Provider>
 );
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    textAlign: 'center',
-    fontWeight: '700',
-  },
-});
