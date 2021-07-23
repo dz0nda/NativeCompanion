f70bd9b95ad69dcec6193931daa543ed8b673047
dz0nda <dzonda@student.42.fr>
2021-07-23 10:41:49 +0200

    init: expo app

--- metro.config.js
+++ metro.config.js
@@ -1,26 +1,4 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
 
-const MetroConfig = require('@ui-kitten/metro-config');
-
-/**
- * @see https://akveo.github.io/react-native-ui-kitten/docs/guides/improving-performance
- */
-const evaConfig = {
-  evaPackage: '@eva-design/eva',
-};
-
-module.exports = MetroConfig.create(evaConfig, {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
-  },
-});
+module.exports = getDefaultConfig(__dirname);
